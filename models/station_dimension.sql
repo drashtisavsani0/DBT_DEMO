820973e2d2ed9055f2b40a3933d50e93e14e2ad7
Drashti Savsani <drashti.savsani@ics-global.in>
2026-01-20 09:18:16 +0000

    trip_facts model created

--- models/station_dimension.sql
+++ models/station_dimension.sql
@@ -1,10 +1,11 @@
 WITH BIKE as(
     select
+    distinct
     start_station_id as station_id,
     start_station_name as station_name,
     start_latitude as station_lat,
     start_longitude as station_lng
-    
+
     from {{ source('demo', 'bike') }}
 )
 
